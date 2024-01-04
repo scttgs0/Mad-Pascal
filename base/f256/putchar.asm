2ba22586f9ea00bd5d15241c9313174b920d84ce
scttgs0 <scttgs0@gmail.com>
2024-01-03 18:57:05 -0600

    putchar routine [from beethead]

--- base/f256/putchar.asm
+++ base/f256/putchar.asm
@@ -1,10 +1,32 @@
 
-/*
+.proc   @putchar (.byte a) .reg
+        pha
+        phx
 
-  PUT CHAR
+        ldx #@iopagectrl(iopPage2)
+        stx IOPAGE_CTRL
 
-*/
+        sta (ScreenPointer)
 
-.proc @putchar (.byte a) .reg
+        inc IOPAGE_CTRL
 
+        lda CursorColor
+        sta (ScreenPointer)
+
+        stz IOPAGE_CTRL
+
+        inc ScreenPointer
+
+        bne @+
+        inc ScreenPointer+1
+        lda ScreenPointer+1             ; lazy check for overrun
+        cmp #>(CS_TEXT_MEM_PTR+80*60)
+        bne @+
+
+        stz ScreenPointer
+        stz ScreenPointer+1
+
+@       plx
+        pla
+        rts
 .endp
