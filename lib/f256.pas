fd92fb625b609da889ed9c4fe442affde4926c0e
scttgs0 <scttgs0@gmail.com>
2024-01-16 01:30:42 -0600

    new bitmap unit

--- lib/f256.pas
+++ lib/f256.pas
@@ -5,22 +5,29 @@ interface
 type
 
     masterctrll = (
-        mcTextOn        = $01,
-        mcOverlayOn     = $02,
-        mcGraphicsOn    = $04,
-        mcBitmapOn      = $08,
-        mcTileMapOn     = $10,
-        mcSpriteOn      = $20,
-        mcGammaOn       = $40);
+        mcTextOn            = $01,
+        mcOverlayOn         = $02,
+        mcGraphicsOn        = $04,
+        mcBitmapOn          = $08,
+        mcTileMapOn         = $10,
+        mcSpriteOn          = $20,
+        mcGammaOn           = $40);
 
     masterctrlh = (
-        mcVideoMode240  = $00,
-        mcVideoMode200  = $01,
-        mcTextDoubleX   = $02,
-        mcTextDoubleY   = $04,
-        mcDisableVideo  = $08,
-        mcTransparent   = $10,
-        mcFontSet1      = $20);
+        mcVideoMode240      = $00,
+        mcVideoMode200      = $01,
+        mcTextDoubleX       = $02,
+        mcTextDoubleY       = $04,
+        mcDisableVideo      = $08,
+        mcTransparent       = $10,
+        mcFontSet1          = $20);
+
+    bitmapctrl = (
+        bmcEnable           = $01,
+        bmcLUT0             = $00,
+        bmcLUT1             = $02,
+        bmcLUT2             = $04,
+        bmcLUT3             = $06);
 
     spritectrl = (
         scEnable            = $01,
