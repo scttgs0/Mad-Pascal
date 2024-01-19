2eebc7b050c889c398641bb227d9dfbcc4b29c99
scttgs0 <scttgs0@gmail.com>
2024-01-18 22:55:17 -0600

    WIP: font unit

--- fnxlibs/font.pas
+++ fnxlibs/font.pas
@@ -1,33 +1,75 @@
 
 unit font;
 
-
 // ------------------------------------
 interface
 // ------------------------------------
 
 uses f256;
 
-procedure Init(idx: byte; addr: dword);
+procedure Init(idx: byte; addr: word); assembler;
 
 
 // ------------------------------------
 implementation
 // ------------------------------------
 
-procedure Init(idx: byte; addr: dword);
-begin
-	IOPAGE_CTRL := iopPage1;
+procedure Init(idx: byte; addr: word); assembler;
+// var
+//     fnt: ^byte absolute $C000;
+//     src: ^byte;
+//     i: word;
+// begin
+//     IOPAGE_CTRL := iopPage1;
 
-    if idx = 0 then begin
-        FONT_MEMORY_BANK0[0] := byte(addr AND $FF);
-        FONT_MEMORY_BANK0[1] := byte((addr shr 8) AND $FF);
-        FONT_MEMORY_BANK0[2] := byte((addr shr 16) AND $FF);
-    end else begin
-        FONT_MEMORY_BANK1[0] := byte(addr AND $FF);
-        FONT_MEMORY_BANK1[1] := byte((addr shr 8) AND $FF);
-        FONT_MEMORY_BANK1[2] := byte((addr shr 16) AND $FF);
-    end;
+//     src := pointer(addr);
+
+//     if idx = 1 then
+//         fnt := fnt + $800;
+
+//     for i := 0 to 1023 do begin
+//         fnt^ := src^;
+//         inc(fnt);
+//         inc(src);
+//     end;
+// end;
+asm
+        phx
+        phy
+
+        ldy IOPAGE_CTRL
+        phy
+
+        ldx #@iopagectrl(iopPage1)
+        stx IOPAGE_CTRL
+
+        lda #>addr
+        sta tempzp+1
+        lda #<addr
+        sta tempzp
+
+        lda #>$C000
+        sta tempzp+3
+        lda #<$C000
+        sta tempzp+2
+
+        ldx #$03
+_1      ldy #$FF
+_2      lda (tempzp),Y
+        sta (tempzp+2),Y
+
+        dey
+        cpy #$FF
+        bne _2
+
+        dex
+        bpl _1
+
+        ply
+        sty IOPAGE_CTRL
+
+        ply
+        plx
 end;
 
 end.
