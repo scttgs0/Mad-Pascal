3627713459f30a3a967fef31a2a857a4961c5089
scttgs0 <scttgs0@gmail.com>
2024-01-20 12:33:46 -0600

    tiletest: test tile scrolling

--- samples/f256/tiletest.pas
+++ samples/f256/tiletest.pas
@@ -4,6 +4,7 @@ program tiletest;
 uses tile, f256;
 
 var
+    x, y, d: smallint;
     tileset00: array[0..8191] of byte = ({$BIN2CSV tile.bin});
     tilemap00: array[0..4299] of byte = ({$BIN2CSV map.bin});
 
@@ -16,4 +17,44 @@ begin
 
     tile.SetInit(0,ord(tilesetctrl.tsVertical),@tileset00);
     tile.MapInit(0,ord(tilemapctrl(tcEnable+tcSmallTiles)),50,43,@tilemap00);
+
+    for x := 0 to 9*16 do begin
+        tile.Scroll(0,x,0);
+
+        for y := 0 to 200 do begin  // delay
+            d := y * 3;
+            if d mod 5 = 0 then
+                d := d + 1;
+        end;
+    end;
+
+    for y := 0 to 13*16 do begin
+        tile.Scroll(0,9*16,y);
+
+        for x := 0 to 200 do begin  // delay
+            d := x * 3;
+            if d mod 5 = 0 then
+                d := d + 1;
+        end;
+    end;
+
+    for x := 0 to 9*16 do begin
+        tile.Scroll(0,9*16-x,13*16);
+
+        for y := 0 to 200 do begin  // delay
+            d := y * 3;
+            if d mod 5 = 0 then
+                d := d + 1;
+        end;
+    end;
+
+    for y := 0 to 13*16 do begin
+        tile.Scroll(0,0,13*16-y);
+
+        for x := 0 to 200 do begin  // delay
+            d := x * 3;
+            if d mod 5 = 0 then
+                d := d + 1;
+        end;
+    end;
 end.
