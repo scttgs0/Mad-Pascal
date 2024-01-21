356d739883cbcfe385f0d65e680b559dea086da9
scttgs0 <scttgs0@gmail.com>
2024-01-20 18:16:02 -0600

    f256: finish tile test

--- samples/f256/tiletest.pas
+++ samples/f256/tiletest.pas
@@ -5,10 +5,18 @@ uses tile, f256;
 
 var
     x, y, d: smallint;
-    tileset00: array[0..8191] of byte = ({$BIN2CSV tile.bin});
-    tilemap00: array[0..4299] of byte = ({$BIN2CSV map.bin});
+
+    palette: array of byte = [$00,$00,$00,$00,$20,$74,26,$00,$6f,$71,$73,$00,$78,$2d,$20,$00,$20,$74,$75,$00,$75,20,$46,$00];
+    tilemap00: array of byte = [{$BIN2CSV map.bin}];
+    tileset00: array of byte = [{$BIN2CSV tile.bin}];
+
 
 begin
+    IOPAGE_CTRL := iopPage1;
+
+    for x := 0 to 23 do
+        Poke($D000+x, palette[x]);
+
     // InitGraph(ord(masterctrll(mcGraphicsOn+mcTileMapOn)),ord(masterctrlh.mcVideoMode240));
     IOPAGE_CTRL := iopPage0;
 
@@ -21,7 +29,7 @@ begin
     for x := 0 to 9*16 do begin
         tile.Scroll(0,x,0);
 
-        for y := 0 to 200 do begin  // delay
+        for y := 0 to 150 do begin  // delay
             d := y * 3;
             if d mod 5 = 0 then
                 d := d + 1;
@@ -31,7 +39,7 @@ begin
     for y := 0 to 13*16 do begin
         tile.Scroll(0,9*16,y);
 
-        for x := 0 to 200 do begin  // delay
+        for x := 0 to 150 do begin  // delay
             d := x * 3;
             if d mod 5 = 0 then
                 d := d + 1;
@@ -41,7 +49,7 @@ begin
     for x := 0 to 9*16 do begin
         tile.Scroll(0,9*16-x,13*16);
 
-        for y := 0 to 200 do begin  // delay
+        for y := 0 to 150 do begin  // delay
             d := y * 3;
             if d mod 5 = 0 then
                 d := d + 1;
@@ -51,7 +59,7 @@ begin
     for y := 0 to 13*16 do begin
         tile.Scroll(0,0,13*16-y);
 
-        for x := 0 to 200 do begin  // delay
+        for x := 0 to 150 do begin  // delay
             d := x * 3;
             if d mod 5 = 0 then
                 d := d + 1;
